1e0cc9cafb989fca8f2d2cee4a8746bebd96b102
Igor Davidovich <sir.funtik18@yandex.ru>
2017-04-30 16:30:03 +0600

    Correction and initWorld

--- inception.cpp
+++ inception.cpp
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
-#include <locale>
 
-main(){
-	setlocale(LC_ALL, "Russian");
-	
-	int i=0,j,n,m;
-	printf("¬ведите n ");
-	scanf("%d",n);
-	printf("¬ведите m ");
-	scanf("%d",m);
-	int a[n][m];
-	for(i=0;i<n;i++){
-		for(j=0;j<m;j++){
-			a[i][j]=1;
-			printf("%4d ",a[i][j]);
+#define WorldH 20
+#define WorldW 20
+
+int World[WorldH][WorldW]={0};
+void initWorld(){
+		for(int h=0;h<WorldW;h++){
+			for(int w=0;w<WorldH;w++){
+				printf("%d ",World[h][w]);
 		}
-		puts("\n");
+		printf("\n");
 	}
+}
+main(){
+initWorld();
+
 	
 return 0;
 }
