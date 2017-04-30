2aa304b185367e2cd3b2d576cefd43c8ea1d47b6
Igor Davidovich <sir.funtik18@yandex.ru>
2017-04-30 16:49:40 +0600

    Something like a random

--- inception.cpp
+++ inception.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
+#include <cstdlib>//random
+#include <ctime>
 
 #define WorldH 20
 #define WorldW 20
@@ -14,9 +16,17 @@ void initWorld(){
 		printf("\n");
 	}
 }
+void random(){
+	bool justice=1;
+		for(int h=0;h<WorldW;h++){
+			for(int w=0;w<WorldH;w++){
+				World[h][w]=rand()%2;
+		}
+	}
+}
 main(){
+srand(time(0));
+random();
 initWorld();
-
-	
 return 0;
 }
