19e05b4d2c370bc52b89b3ffa2d97ffe5b3da2ae
hazker <zov.zon@gmail.com>
2017-05-04 22:06:32 +0700

    Add function life

--- inception.cpp
+++ inception.cpp
@@ -29,7 +29,7 @@ void printWorld(){
 	for(int h=0;h<WorldW;h++){
 		for(int w=0;w<WorldH;w++){
 			if(World[h][w].live==1){
-				printf("o");
+				printf("#");
 			}else{
 				printf(" ");
 			}
@@ -37,9 +37,64 @@ void printWorld(){
 		printf("\n");
 	}
 }
+
+void life(){
+	int l=0;
+		for(int h=0;h<WorldW;h++){
+			for(int w=0;w<WorldH;w++){
+			if(World[h+1][w+1].live==1){
+				l++;
+			}
+			if(World[h][w+1].live==1){
+				l++;
+			}
+			if(World[h+1][w].live==1){
+				l++;
+			}
+			if(World[h-1][w+1].live==1){
+				l++;
+			}
+			if(World[h+1][w-1].live==1){
+				l++;
+			}
+			if(World[h-1][w].live==1){
+				l++;
+			}
+			if(World[h][w-1].live==1){
+				l++;
+			}
+			if(World[h][w+1].live==1){
+				l++;
+			}
+			if(World[h][w+1].live==1){
+				l++;
+			}
+			if(World[h-1][w-1].live==1){
+				l++;
+			}
+			if(l==3 && World[h][w].live==0){
+				World[h][w].live=1;
+			}
+			if((l==2 || l==3) && World[h][w].live==1){
+				World[h][w].live=1;
+			}
+			else{
+				World[h][w].live=0;
+			}
+		}
+	}
+}
+
 main(){
 srand(time(0));
 initWorld();
 printWorld();
+system("PAUSE");
+while(1){
+	puts("\n");
+	life();
+	printWorld();
+	system("PAUSE");
+}
 return 0;
 }
