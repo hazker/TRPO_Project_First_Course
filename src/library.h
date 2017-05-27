20816c6a955cf55694c3d88d66729d6338cacdb0
hazker <zov.zon@gmail.com>
2017-05-27 19:05:27 +0700

    Add struct.h

--- src/library.h
+++ src/library.h
@@ -1,15 +1,6 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
-#define WorldH 40
-#define WorldW 40
-
-struct Point {
-	unsigned live : 1;
-};
-
-struct Point World[WorldH][WorldW] = { 0 };
-struct Point CopyWorld[WorldH][WorldW] = { 0 };
 
 int initWorld(struct Point world[WorldH][WorldW]) {
 	int h,w;
