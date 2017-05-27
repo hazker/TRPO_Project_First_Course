623987a04c4b3e25e4cb77ea86e980ad99971c01
hazker <zov.zon@gmail.com>
2017-05-27 22:47:44 +0700

    Add game rules

---
+++ src/rules.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define N 80
+
+void rules(){
+	char str[N];
+	FILE *f;
+	f=fopen("rules/rules.txt","r");
+	while(fgets(str,N,f)!=NULL)
+		printf("%s",str);
+		printf("\n");
+		fclose(f);
+}
