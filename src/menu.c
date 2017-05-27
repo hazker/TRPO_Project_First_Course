0c5444ea52035427c1b55d806bd5923889d0a6b1
hazker <zov.zon@gmail.com>
2017-05-27 21:24:58 +0700

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -3,15 +3,15 @@ int var=0;
 int menu(){
 	while(1){
 		system("cls");
-		printf("Menu\n");
-		printf("1.Play\n");
-		printf("2.Rules of the game\n");
-		printf("3.Exit\n");
-		printf("Select the menu item: ");
+		printf("Меню\n");
+		printf("1.Играть\n");
+		printf("2.Правила игры\n");
+		printf("3.Выход\n");
+		printf("Выбор пункта меню: ");
 		scanf("%d", &var);
 		if(var>0 && var<4)break;
 		else{
-			printf("The wrong menu item is selected, choose again\n");
+			printf("Выбран неправильный пункт меню, \n");
 		}
 	}
 	return 0;
