9b7ed4fda51ba0bd90892c03b2a8d1425bf51d84
hazker <zov.zon@gmail.com>
2017-05-30 11:13:49 +0700

    Update rules.c

--- src/CFiles/rules.c
+++ src/CFiles/rules.c
@@ -3,14 +3,14 @@
 #define N 80
 
 int rules(){
-	char str[N];
-	FILE *f;
-	f=fopen("rules/rules.txt","r");
-	if (fgets(str,N,f)==NULL)
-	return 0;
-	while(fgets(str,N,f)!=NULL)
-		printf("%s",str);
-		printf("\n");
-		fclose(f);
+	//char str[N];
+	//FILE *f;
+	////f=fopen("rules/rules.txt","r");
+	//if (fgets(str,N,f)==NULL)
+	//return 0;
+	//while(fgets(str,N,f)!=NULL)
+		//printf("%s",str);
+		//printf("\n");
+		//fclose(f);
 return 1;
 }
