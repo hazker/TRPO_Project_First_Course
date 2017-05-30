1a32016c2c28a3ea65d7ca2a8c0f57ed6edc5455
hazker <zov.zon@gmail.com>
2017-05-30 11:03:59 +0700

    Update validation_test.c

--- test/validation_test.c
+++ test/validation_test.c
@@ -2,7 +2,7 @@
 
 #include "ctest.h"
 
-int orld[40][40]={0};
+/*int orld[40][40]={0};
 
 CTEST(Cheak, InitWorld)
 {
@@ -19,13 +19,13 @@ CTEST(Cheak, PrintWorld)
 	ASSERT_EQUAL(expected, result);
 }
 
-/*CTEST(Cheak, CopyPast)
+CTEST(Cheak, CopyPast)
 {
 	initWorld(World);
 	int result = CopyPast(CopyWorld,World);
 	int expected = 0;
 	ASSERT_EQUAL(expected, result);
-}*/
+}
 
 CTEST(Cheak, LogicLifeFor2)
 {
@@ -51,7 +51,7 @@ CTEST(Cheak, LogicLifeNotLifeFail)
   	if(result<=1 || result>=4){
 	expected=result ;}
 	ASSERT_EQUAL(expected, result);
-}
+}*/
 CTEST(Cheak, RulesOpenTrue)
  {
 	int result = rules();
